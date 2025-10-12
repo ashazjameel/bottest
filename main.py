83bcaa753247f16671d17d4c1c013aa8a6c8b102
ashazjameel <161173725+ashazjameel@users.noreply.github.com>
2025-10-12 18:45:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='/', intents=intents)
-tree = app_commands.CommandTree(client)
+tree = bot.tree
 
 @bot.event
 async def on_ready():
