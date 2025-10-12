4d754ebf91808893fb21cbb2461ca6e8fa07ca17
ashazjameel <161173725+ashazjameel@users.noreply.github.com>
2025-10-12 18:04:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ TOKEN = os.environ['BOT_TOKEN']  # Fetch token from Render
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='/', intents=intents)
 
 @bot.event
 async def on_ready():
@@ -15,6 +15,31 @@ async def on_ready():
 
 @bot.command()
 async def hello(ctx):
+    ep = "j1nermw1"
+    ep_ce = "9do8wj31"
+
+
+    username = "Emmir44"# "William_Swordsmith"
+    userid = GetUserSummary(username).perform().user.id
+
+    ldr = GetUserLeaderboard(userid).perform()
+    runs = ldr.runs
+
+    ep_ldr = {1:0,2:0,3:0}
+    epce_ldr = {1:0,2:0,3:0}
+
+    for i in runs:
+        if i.place != None and (i.gameId == ep or i.gameId == ep_ce):
+            place = i.place
+            if i.gameId == ep:
+                if 1 <= place <= 3:
+                    ep_ldr[place] += 1             
+            elif i.gameId == ep_ce:
+            
+                if 1 <= place <= 3:
+                    epce_ldr[place] += 1
+                    
+    print("Username:",username,"Entry Point",ep_ldr,"Entry Point Category Extensions",epce_ldr)
     await ctx.send("Hello from Render!")
 
 bot.run(TOKEN)
