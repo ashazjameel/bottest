c4cec119cd36abbfac24cb82302fd7429251c5b9
ashazjameel <161173725+ashazjameel@users.noreply.github.com>
2025-10-12 18:21:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -46,6 +46,11 @@ async def hello(interaction: discord.Interaction):
                 if 1 <= place <= 3:
                     epce_ldr[place] += 1
                     
-    await interaction.response.send_message("Username: "+username+" Entry Point "+ep_ldr+" Entry Point Category Extensions "+epce_ldr)
+    await interaction.response.send_messageawait interaction.response.send_message(
+    f"Username: {username}\n"
+    f"Entry Point: {ep_ldr}\n"
+    f"Entry Point Category Extensions: {epce_ldr}"
+)
+
 
 bot.run(TOKEN)
