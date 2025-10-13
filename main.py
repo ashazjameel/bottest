ce0ef9c5356cb7f6c27cc9cc6592988067b62919
ashazjameel <161173725+ashazjameel@users.noreply.github.com>
2025-10-13 19:09:21 +0100

    .

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ async def first_command(interaction: discord.Interaction):
 )"""
 
 @bot.command()
-async def statlookup(interaction: discord.Interaction, username: str):#ctx, username: str):
+async def statlookup(ctx, username: str): #interaction: discord.Interaction, username: str):
     if username is None:
         #await interaction.response.send_message("no username provided")
         await ctx.send("no username provided")
