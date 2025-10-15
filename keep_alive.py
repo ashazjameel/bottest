b32733facfad09649746987bec3c3beef1ea025c
ashazjameel <161173725+ashazjameel@users.noreply.github.com>
2025-10-15 19:17:23 +0100

    Update and rename keep_alive.js to keep_alive.py

---
+++ keep_alive.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from threading Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is running"
+
+def run():
+    app.run(host="0.0.0.0", port=8080)
+
+def keep_alive():
