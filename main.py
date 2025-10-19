90ada21f82b662da9c1e8a4f9dd364257d244748
ashazjameel <161173725+ashazjameel@users.noreply.github.com>
2025-10-19 16:58:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -92,10 +92,7 @@ async def statlookup(interaction: discord.Interaction, username: str):#(ctx, use
     countryId = ""
     for i in country:
         countryId += countries[i.lower()]
-    country = f"flag_{country}"
-    countryId = "🇬🇧"     #test
-    #country = bot.get_emoji(countryId)
-    country = f"<:{country}:{countryId}>"
+    #countryId = "🇬🇧"     #test
     
     #link = "/static/user/j9516yv8/image.jpg?v=e87ae57"
     ldr = await GetUserLeaderboard(userid).perform_async()
@@ -127,7 +124,7 @@ async def statlookup(interaction: discord.Interaction, username: str):#(ctx, use
     description = f"Entry Point:‎‎ {b} {first} {ep_ldr[1]} {b} {second} {ep_ldr[2]} {b} {third} {ep_ldr[3]} {b} {fourth} {ep_ldr[4]} \n Entry Point Category Extensions: {b} {first} {epce_ldr[1]} {b} {second} {epce_ldr[2]} {b} {third} {epce_ldr[3]} {b} {fourth} {epce_ldr[4]}"
     title = f"{username}'s stats"
     embed = discord.Embed(title=title,description=description,color=int(colour, 16))
-    embed.set_author(name=f"{username} {countryId}", icon_url=f"https://www.speedrun.com{link}") #"https://www.speedrun.com/static/user/j9516yv8/image.jpg?v=e87ae57")
+    embed.set_author(name=f"{username} {first}", icon_url=f"https://www.speedrun.com{link}") #"https://www.speedrun.com/static/user/j9516yv8/image.jpg?v=e87ae57")
     #embed.set_author(name=username, icon_url="https://www.speedrun.com/static/user/j9516yv8/image.jpg?v=e87ae57")   good
     #embed.set_thumbnail(url="https://www.speedrun.com/static/user/j9516yv8/image.jpg?v=e87ae57")     ts more like a thumbnail icl
     #embed.add_field(name="Field1", value="hi", inline=False)
