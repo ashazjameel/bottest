41530ca5048fedd690b9ead062fba5e4a2ac041f
ashazjameel <161173725+ashazjameel@users.noreply.github.com>
2025-10-24 00:26:22 +0100

    typo

--- main.py
+++ main.py
@@ -90,7 +90,7 @@ async def statlookup(interaction: discord.Interaction, username: str):#(ctx, use
     else:
         link = "/static/user/qjo0qgnj/image.png?v=44e25be"
     f = Image.open(BytesIO(requests.get(f"https://www.speedrun.com{link}").content))
-    image_link = f"https://cdn.filestackcontent.com/{IMAGE_API_KEY}/resize=width:{min(f.size},height:{min(f.size)},fit:crop/https://www.speedrun.com{link}"
+    image_link = f"https://cdn.filestackcontent.com/{IMAGE_API_KEY}/resize=width:{min(f.size)},height:{min(f.size)},fit:crop/https://www.speedrun.com{link}"
     #test
     #image_link = "https://cdn.filestackcontent.com/AA3hldwYkSYG026IjoXhYz/resize=width:400,height:400,fit:crop/https://www.speedrun.com/static/user/qjo0qgnj/image.png?width=600&height=4600"
     country = link1.user.areaId.split("/")[0]
