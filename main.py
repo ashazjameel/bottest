532a7c53c0aa0566464765f6429e29a4a01d9093
ashazjameel <161173725+ashazjameel@users.noreply.github.com>
2025-10-26 16:21:30 +0000

    Update main.py

--- main.py
+++ main.py
@@ -165,7 +165,7 @@ async def leaderboard(interaction: discord.Interaction):
         for x in plyr:
             if not(x.id in player_list):
                 player_list.append(x.id)
-    await msg.edit(f"Total players: {len(player_list)}")
+    await msg.edit(content=f"Total players: {len(player_list)}")
 
     for i in range(len(player_list)):
         userID = player_list[i]
@@ -186,6 +186,6 @@ async def leaderboard(interaction: discord.Interaction):
                         epce_ldr[place] += 1
         score = ep_ldr[1]*4+ep_ldr[2]*3+ep_ldr[3]*2+ep_ldr[4]*1+epce_ldr[1]*4+epce_ldr[2]*3+epce_ldr[3]*2+epce_ldr[4]*1
         score_list.append((userID,score))
-        await msg.edit(f"Loading user stats [{i+1}/{len(player_list)}]")
+        await msg.edit(content=f"Loading user stats [{i+1}/{len(player_list)}]")
     
 bot.run(TOKEN)
