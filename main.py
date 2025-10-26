3b3c0d0d951c508a94981bfe3f6ece999c25bf93
ashazjameel <161173725+ashazjameel@users.noreply.github.com>
2025-10-26 16:06:25 +0000

    indent fix

--- main.py
+++ main.py
@@ -166,23 +166,23 @@ async def leaderboard(interaction: discord.Interaction):
                 player_list.append(x.id)
     msg = await interaction.followup.send(f"Total players: {len(player_list)}")
 
-for i in range(len(player_list)):
-    userID = player_list[i]
-    ldr = await GetUserLeaderboard(userID).perform_async()
-    runs = ldr.runs
-    username = ldr.user.name
-    ep_ldr = {1:0,2:0,3:0,4:0}
-    epce_ldr = {1:0,2:0,3:0,4:0}
+    for i in range(len(player_list)):
+        userID = player_list[i]
+        ldr = await GetUserLeaderboard(userID).perform_async()
+        runs = ldr.runs
+        username = ldr.user.name
+        ep_ldr = {1:0,2:0,3:0,4:0}
+        epce_ldr = {1:0,2:0,3:0,4:0}
     
-    for i in runs:
-        if i.place != None and (i.gameId == ep or i.gameId == ep_ce):
-            place = i.place
-            if i.gameId == ep:
-                if 1 <= place <= 4:
-                    ep_ldr[place] += 1             
-            elif i.gameId == ep_ce:
-                if 1 <= place <= 4:
-                    epce_ldr[place] += 1
+        for i in runs:
+            if i.place != None and (i.gameId == ep or i.gameId == ep_ce):
+                place = i.place
+                if i.gameId == ep:
+                    if 1 <= place <= 4:
+                        ep_ldr[place] += 1             
+                elif i.gameId == ep_ce:
+                    if 1 <= place <= 4:
+                        epce_ldr[place] += 1
     score = ep_ldr[1]*4+ep_ldr[2]*3+ep_ldr[3]*2+ep_ldr[4]*1+epce_ldr[1]*4+epce_ldr[2]*3+epce_ldr[3]*2+epce_ldr[4]*1
     score_list.append((userID,score))
     await msg.edit(f"Loading user stats [{i+1}/{len(player_list)}]")
